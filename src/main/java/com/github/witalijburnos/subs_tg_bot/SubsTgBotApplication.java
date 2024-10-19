d8905f856107df5da4cec443fad136f1463136fc
Witalij B. <burnos.witalik@gmail.com>
2024-10-19 23:07:56 +0200

    added common telegram bot to project

--- src/main/java/com/github/witalijburnos/subs_tg_bot/SubsTgBotApplication.java
+++ src/main/java/com/github/witalijburnos/subs_tg_bot/SubsTgBotApplication.java
@@ -2,8 +2,12 @@ package com.github.witalijburnos.subs_tg_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"com.github.witalijburnos.subs_tg_bot",
+		"org.telegram.telegrambots"
+})
 public class SubsTgBotApplication {
 
 	public static void main(String[] args) {
