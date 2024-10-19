93ea825d05267209028d912544e98f7a07be1dce
Witalij B. <burnos.witalik@gmail.com>
2024-10-19 20:40:30 +0200

    added Spring Boot skeleton to project

---
+++ src/main/java/com/github/witalijburnos/subs_tg_bot/SubsTgBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.witalijburnos.subs_tg_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubsTgBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/witalijburnos/subs_tg_bot/SubsTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.witalijburnos.subs_tg_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubsTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
